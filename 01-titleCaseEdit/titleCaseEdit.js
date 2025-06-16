ed4d1aec400d2d8bc48bd88e127675c7ac8ed961
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-15 23:02:07 -0400

    Completed 01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  const words = title.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    words[i] = word[0].toUpperCase() + word.slice(1);
+  }
+
+  return words.join(" ");
 }
 
 // Do not edit this line;
