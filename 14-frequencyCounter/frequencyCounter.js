ac22de49d0b3bd9bc5f679cc1a1f7fea77a9b25b
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-15 22:28:48 -0400

    Completed 14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  const freq = {};
+  for (let i = 0; i < word.length; i++) {
+    let key = word[i];
+    if (freq[key]) {
+      freq[key] += 1;
+    } else {
+      freq[key] = 1;
+    }
+ }
+  return freq;
 }
 
 // Do not edit this line;
