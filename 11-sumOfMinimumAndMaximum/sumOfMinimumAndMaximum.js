5757934c7fb5cdb15a9b68d9069e2abfe7021145
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-06-15 22:27:34 -0400

    Completed 11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,19 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  var lowest = nums[0];
+  var highest = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (lowest > nums[i]) {
+      lowest = nums[i];
+    }
+    if (highest < nums[i]) {
+      highest = nums[i];
+    }
+  }
+
+  var sum = lowest + highest;
+
+  return sum;
 }
 
 // Do not edit this line;
